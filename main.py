bbc5687b834de2be80543c74f1d9909df9cc20d5
chriscarol18may <chriscarol18may@gmail.com>
2024-09-17 11:54:41 +0530

    intial

---
+++ main.py
@@ -0,0 +1,9 @@
+#below statement takes input from user
+number=int(input("Enter a number   "))
+
+#below statement is conditional and check for the condition to be true or false
+if number==103:
+  print("Call the Fire Brigade")
+else:
+  print("Dont call fire brigade")
+
